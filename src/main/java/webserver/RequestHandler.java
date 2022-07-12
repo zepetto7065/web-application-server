20c458b0a2b45d6f5bbbd956ba6be6c2cc664593
유상문 <sangmoon.yoo@wadiz.kr>
2022-07-12 15:26:40 +0900

    사용자 list

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,9 +35,13 @@ public class RequestHandler extends Thread {
             }
             String url = HttpRequestUtils.getUrl(line);
             Map<String, String> headers = new HashMap<>();
+            boolean logined = false;
             while (!"".equals(line)) {
                 log.debug("header : {}", line);
                 line = br.readLine();
+                if(line.contains("Cookie")){
+                    logined = isLogin(line);
+                }
                 String[] headerTokens = line.split(": ");
                 if (headerTokens.length == 2) {
                     headers.put(headerTokens[0], headerTokens[1]);
@@ -74,6 +79,27 @@ public class RequestHandler extends Thread {
                     DataOutputStream dos = new DataOutputStream(out);
                     response302HeaderWithCookie(dos, "logined=true");
                 }
+            }else if (url.equals("/user/list")){
+                if(!logined){
+                    log.debug("User N ot login!");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302Header(dos);
+                }
+                Collection<User> users = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border='1'>");
+                for (User user : users) {
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+                byte[] body = sb.toString().getBytes();
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             }else if(url.endsWith("css")){
                 DataOutputStream dos = new DataOutputStream(out);
                 byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
@@ -89,6 +115,16 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean isLogin(String line) {
+        String[] headerTokens = line.split(": ");
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if(value == null){
+            return false;
+        }
+        return Boolean.parseBoolean(value);
     }
 
     private void response302HeaderWithCookie(DataOutputStream dos, String cookie) {
